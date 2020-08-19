4f8582f91db03a742e4ae5a30b06416f4a9def17
Emanuel Kluge <mail@emanuel-kluge.de>
2020-08-19 19:40:51 +0200

    test: check 404 page

--- server/http/get-index/test.ts
+++ server/http/get-index/test.ts
@@ -10,4 +10,13 @@ describe('handler()', () => {
     expect(headers['content-type']).toBe('text/html; charset=utf8');
     expect(body.includes(headline)).toBe(true);
   });
+
+  it('should return a 404 page', async () => {
+    const pathParameters = { proxy: '/does/not/exist' };
+    const { statusCode, body } = await handler({ pathParameters });
+    const headline = 'Not found.';
+
+    expect(statusCode).toBe(404);
+    expect(body.includes(headline)).toBe(true);
+  });
 });
