f47f6f4baff29c4a5ee85bfa211c639c938ebe2e
Emanuel Kluge <mail@emanuel-kluge.de>
2020-09-19 17:12:57 +0200

    chore: prepare for future changes of tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,10 @@ module.exports = {
     enabled: process.env.NODE_ENV === 'production',
     content: ['./server/**/*.tsx'],
   },
+  future: {
+    purgeLayersByDefault: true,
+    removeDeprecatedGapUtilities: true,
+  },
   theme: {},
   variants: {},
   plugins: [],
