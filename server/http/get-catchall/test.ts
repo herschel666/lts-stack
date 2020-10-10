bec4870163500169bf1481d421caf85b3e797aa8
Emanuel Kluge <mail@emanuel-kluge.de>
2020-10-10 13:13:56 +0200

    test: update test cases

---
+++ server/http/get-catchall/test.ts
@@ -0,0 +1,11 @@
+import { handler } from '.';
+
+describe('handler()', () => {
+  it('should return a 404 page', async () => {
+    const { statusCode, body } = await handler();
+    const headline = 'Not found.';
+
+    expect(statusCode).toBe(404);
+    expect(body.includes(headline)).toBe(true);
+  });
+});
--- server/http/get-index/test.ts
+++ server/http/get-index/test.ts
@@ -2,21 +2,12 @@ import { handler } from '.';
 
 describe('handler()', () => {
   it('should return a valid response', async () => {
-    const { headers, body } = await handler({ pathParameters: null });
+    const { headers, body } = await handler();
     const headline =
       'A modern stack consisting of Lambda, Turbolinks &amp; StimulusJS.';
 
     expect(headers['cache-control'].includes('no-cache')).toBe(true);
     expect(headers['content-type']).toBe('text/html; charset=utf8');
-    expect(body.includes(headline)).toBe(true);
-  });
-
-  it('should return a 404 page', async () => {
-    const pathParameters = { proxy: '/does/not/exist' };
-    const { statusCode, body } = await handler({ pathParameters });
-    const headline = 'Not found.';
-
-    expect(statusCode).toBe(404);
     expect(body.includes(headline)).toBe(true);
   });
 });
