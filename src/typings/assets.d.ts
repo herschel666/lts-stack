962de5e669148562a4c1258b122fc896b2bc9bbc
Emanuel Kluge <mail@emanuel-kluge.de>
2020-12-20 09:14:57 +0100

    chore: remove superfluous asset types

--- src/typings/assets.d.ts
+++
@@ -1,8 +0,0 @@
-declare module '*.jpg';
-declare module '*.png';
-declare module '*.gif';
-declare module '*.webp';
-declare module '*.svg';
-declare module '*.css';
-declare module '*.html';
-declare module '*.json';
