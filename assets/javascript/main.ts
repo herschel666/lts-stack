740bd36b11eec283d74def4d10b33e697ba1382e
Emanuel Kluge <mail@emanuel-kluge.de>
2020-12-20 09:15:32 +0100

    fix: initialize Stimulus correctly

--- assets/javascript/main.ts
+++ assets/javascript/main.ts
@@ -3,7 +3,7 @@ import { Application } from 'stimulus';
 import { definitionsFromContext } from 'stimulus/webpack-helpers';
 
 const application = Application.start();
-const context = require.context('./controllers', false, /\.js$/);
+const context = require.context('./controllers', false, /\.ts$/);
 
 application.load(definitionsFromContext(context));
 
